8c69dbf6b0c7969fceab1f4d5d3e8f507bad556e
jamcatbiz <james.cate.biz@gmail.com>
2025-02-17 22:10:01 -0500

    feat: improving onboarding experience

--- app/src/routes/(admin)/account/select_plan/+page.svelte
+++ app/src/routes/(admin)/account/select_plan/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import PricingHelp from "../../../(marketing)/subscriptions/PricingHelp.svelte"
   import PricingModule from "../../../(marketing)/subscriptions/PricingModule.svelte"
 </script>
 
@@ -6,17 +7,16 @@
   <title>Select a Plan</title>
 </svelte:head>
 
-<div
-  class="text-center content-center min-h-[100vh] pb-12 mt-4 flex items-center place-content-center"
->
+<div class="min-h-[70vh] pb-8 pt-[5vh] px-4">
+  <h1 class="text-3xl font-bold text-center">Treat Yo' Self</h1>
+  <h2 class="text-xl text-center opacity-50 mt-1 pb-3">
+    And support your favorite games too!
+  </h2>
   <div class="flex flex-col w-full px-6">
     <div>
       <h1 class="text-2xl font-bold mb-2">Select a Plan</h1>
-      <div class="mb-6">
-        View our <a href="/pricing" target="_blank" class="link">pricing page</a
-        > for details.
-      </div>
-      <PricingModule callToAction="Select Plan" />
+      <PricingModule callToAction="Get Started" highlightedPlanId="subscriber" />
+      <PricingHelp />
     </div>
   </div>
 </div>
--- app/src/routes/(marketing)/login/login_config.ts
+++ app/src/routes/(marketing)/login/login_config.ts
@@ -1,7 +1,7 @@
 import { ThemeSupa } from "@supabase/auth-ui-shared"
 import type { Provider } from "@supabase/supabase-js"
 
-export const oauthProviders = ["github"] as Provider[]
+export const oauthProviders = [] as Provider[]
 
 // use the css variables from DaisyUI to style Supabase auth template
 export const sharedAppearance = {
@@ -10,14 +10,24 @@ export const sharedAppearance = {
     default: {
       colors: {
         brand: "oklch(var(--p))",
-        brandAccent: "oklch(var(--ac))",
-        inputText: "oklch(var(--color-base-content))",
+        brandAccent: "oklch(var(--p))",
         brandButtonText: "oklch(var(--pc))",
-        messageText: "oklch(var(--b))",
+        defaultButtonBackground: "oklch(var(--p))",
+        defaultButtonBackgroundHover: "oklch(var(--p))",
+        defaultButtonBorder: 'transparent',
+        defaultButtonText: "oklch(var(--pc))",
         dividerBackground: "oklch(var(--n))",
-        inputLabelText: "oklch(var(--color-base-content))",
-        defaultButtonText: "oklch(var(--n))",
-        anchorTextColor: "oklch(var(--p))",
+        inputBackground: 'transparent',
+        inputBorder: "oklch(var(--n))",
+        inputBorderHover: "oklch(var(--nc))",
+        inputBorderFocus: "oklch(var(--nc))",
+        inputText: "oklch(var(--bc))",
+        inputLabelText: "oklch(var(--bc))",
+        inputPlaceholder: "oklch(var(--n))",
+        messageText: "oklch(var(--suc))",
+        messageTextDanger: "oklch(var(--erc))",
+        anchorTextColor: "oklch(var(--bc))",
+        anchorTextHoverColor: "oklch(var(--bc))",
       },
       fontSizes: {
         baseInputSize: "16px",
